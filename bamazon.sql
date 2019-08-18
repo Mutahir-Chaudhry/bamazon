b08a4fc31045e21e97f56f40e91ab5a1a9b11152
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-18 13:01:05 -0400

    Used INSERT INTO statements to add data for 10 products in the products table within the bamazon_db. Successfully ran query in MySQL workbench.

--- bamazon.sql
+++ bamazon.sql
@@ -13,3 +13,34 @@ CREATE TABLE products (
   quantity INT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Bat", "Sports", 40.00, 5)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Basket Ball", "Sports", 20.00, 3)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Vitamin C", "Health", 10.00, 8)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Corn", "Food", 5.00, 7)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Banana", "Food", 4.00, 6)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Bread", "Food", 7.00, 3)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("AA Batteries", "Electronics", 13.00, 4)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Plate", "Houseware", 24.00, 12)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Headphones", "Electronics", 60.00, 2)
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Spoons", "Houseware", 7.00, 10)
+
