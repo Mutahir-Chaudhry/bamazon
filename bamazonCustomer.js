f938fcd8010e89a217eb264e9ab793fa85f0c1b7
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-18 17:45:34 -0400

    Decided to take a different route. Combined everything into one function called buyProduct().

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,32 +13,84 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    showProduct();
+    buyProduct();
 });
 
+var cartArray = [];
 
-function showProduct() {
-    console.log("Here are all the products we have to offer!\n");
-    connection.query("SELECT * FROM products", function (err, res) {
+function buyProduct(){
+    connection.query("SELECT * FROM products", function(err, results){
         if (err) throw err;
-        console.log(res);
-        connection.end();
-        start();
+        inquirer
+        .prompt([
+            {
+                name:"choice",
+                type: "rawlist",
+                choices: function() {
+                    var choicesArray = [];
+                    for (var i = 0; i < results.length; i++){
+                        choicesArray.push(results[i].product_name);
+                    }
+                    return choicesArray;
+                },
+                message: "What product would you like to purchase?"
+            },
+            {
+                name:"quantity",
+                type: "input",
+                message: "What quantity of this product would you like to purchase?"
+            }
+        ]).then(function(answer){
+            var chosenProduct;
+            for (var i = 0; i <results.length; i++){
+                if(results[i].product_name === answer.choice){
+                    chosenProduct = results[i];
+                } 
+            }
+            if (chosenProduct.quantity > parseInt(answer.quantity)){
+               connection.query(
+                   "UPDATE products SET ? WHERE ?",
+                   [
+                       {
+                           quantity: chosenProduct.quantity - answer.quantity
+                       },
+                       {
+                           item_id: chosenProduct.item_id
+                       }
+                   ], function(err){
+                       if (err) throw err;
+                       var price = answer.quantity * chosenProduct.price;
+                       inquirer
+                       .prompt([
+                           {
+                               name:"action",
+                               type:"rawlist",
+                               message:"Would you like to keep shopping?",
+                               choices:[
+                                   "Yes",
+                                   "No"
+                                ]
+                           }
+                       ]).then(function(answer){
+                           if (answer.action === "Yes"){
+                            cartArray.push(price)
+                            console.log("Enjoy your shopping!\n")
+                            buyProduct();
+                           }else{
+                            cartArray.push(price)
+                            var add = (a, b) => a + b
+                            var total = cartArray.reduce(add)
+                            console.log("We appreciate your business! Your order will be shipped immediately and will arrive in the next 24 hrs! \nYour total cost today is : $" + (total) + "\n");
+                            connection.end();
+                           }
+                       })
+                   }
+               );
+            }else {
+                console.log("Sorry, we don't have that many in stock :(")
+                buyProduct();
+            }
+        });
     });
 }
 
-function start() {
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What is the item_id of the product you would like to buy?"
-            },
-            {
-                name: "quantity",
-                type: "input",
-                message: "How many units of the product would you like to buy?"
-            }]
-        )
-}
