8a89fd0f2be1a717ba2a264c72e30fff3968e43c
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-18 13:28:49 -0400

    Added inquirer variable and ran npm install inquirer. Created initial start(); to run after showProducts(); where inquirer asks the customer the item_id of the product they would like to buy, and then the quantity.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,44 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const inquirer = require("inquirer")
+
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "MySQLPassword",
     database: "bamazon_db"
-  });
+});
 
-  connection.connect(function(err){
-      if (err) throw err;
-      console.log("connected as id " + connection.threadId + "\n");
-      showProduct();
-  });
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    showProduct();
+});
 
 
-  function showProduct() {
+function showProduct() {
     console.log("Here are all the products we have to offer!\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+        start();
     });
-  }
-  
+}
+
+function start() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "input",
+                message: "What is the item_id of the product you would like to buy?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units of the product would you like to buy?"
+            }]
+        )
+}
